4a11c7159afe7f2f089737929e3eff0cc0d0a483
Octavian Ciubotaru <octavian@ciubotaru.net>
2019-08-20 17:09:54 +0300

    AD3-24: Removed references to reports and mongo modules

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/AbstractEditPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/AbstractEditPage.java
@@ -40,7 +40,6 @@ import org.devgateway.toolkit.forms.wicket.components.form.SummernoteBootstrapFo
 import org.devgateway.toolkit.forms.wicket.page.BasePage;
 import org.devgateway.toolkit.persistence.dao.GenericPersistable;
 import org.devgateway.toolkit.persistence.service.BaseJpaService;
-import org.devgateway.toolkit.reporting.spring.util.ReportsCacheService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -109,9 +108,6 @@ public abstract class AbstractEditPage<T extends GenericPersistable & Serializab
     private EntityManager entityManager;
 
     @SpringBean(required = false)
-    private ReportsCacheService reportsCacheService;
-
-    @SpringBean(required = false)
     private MarkupCacheService markupCacheService;
 
     public EditForm getEditForm() {
@@ -123,10 +119,6 @@ public abstract class AbstractEditPage<T extends GenericPersistable & Serializab
     }
 
     public void flushReportingCaches() {
-        if (reportsCacheService != null) {
-            reportsCacheService.flushCache();
-        }
-
         if (markupCacheService != null) {
             markupCacheService.flushMarkupCache();
             markupCacheService.clearPentahoReportsCache();
