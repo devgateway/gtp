c2bcea06f00c710bf23d3f8348c27b8bb7339de4
sdimunzio <sdimunzio@gmail.com>
2019-10-22 12:13:53 -0300

    synchronising config language with current selected language

---
+++ ui20/src/translations/messages.js
@@ -0,0 +1,316 @@
+import {
+  defineMessages
+} from 'react-intl'
+
+const messages = defineMessages({
+
+  stories_title_1: {
+    id: "stories.title_1",
+    defaultMessage: "Main crops by zone"
+  },
+
+  stories_subtitle_1: {
+    id: "stories.subtitle_1",
+    defaultMessage: "A short descriptive line will go here"
+  },
+
+  stories_title_2: {
+    id: "stories.title_2",
+    defaultMessage: "Main crops by zone"
+  },
+
+  stories_subtitle_2: {
+    id: "stories.subtitle_2",
+    defaultMessage: "A short descriptive line will go here"
+  },
+
+  stories_title_3: {
+    id: "stories.title_3",
+    defaultMessage: "Main crops by zone"
+  },
+
+  stories_subtitle_3: {
+    id: "stories.subtitle_3",
+    defaultMessage: "A short descriptive line will go here"
+  },
+
+  stories_title_4: {
+    id: "stories.title_4",
+    defaultMessage: "Main crops by zone"
+  },
+
+  stories_subtitle_4: {
+    id: "stories.subtitle_4",
+    defaultMessage: "A short descriptive line will go here"
+  },
+
+  stories_title_5: {
+    id: "stories.title_5",
+    defaultMessage: "Main crops by zone"
+  },
+
+  stories_subtitle_5: {
+    id: "stories.subtitle_5",
+    defaultMessage: "A short descriptive line will go here"
+  },
+
+
+
+  weather_title_1: {
+    id: "weather.title_1",
+    defaultMessage: "Main crops by zone"
+  },
+  weather_subtitle_1: {
+    id: "weather.subtitle_1",
+    defaultMessage: "This is where the short descriptive line will go here to describe the category"
+  },
+
+  weather_title_2: {
+    id: "weather.title_2",
+    defaultMessage: "Meteorological data"
+  },
+
+  weather_subtitle_2: {
+    id: "weather.subtitle_2",
+    defaultMessage: "Weather forecast for next 10 days"
+  },
+
+  weather_title_3: {
+    id: "weather.title_3",
+    defaultMessage: "Main crops by zone"
+  },
+
+  weather_subtitle_3: {
+    id: "weather.subtitle_3",
+    defaultMessage: "This is where the short descriptive line will go here to describe the category"
+  },
+
+  weather_title_4: {
+    id: "weather.title_4",
+    defaultMessage: "Main crops by zone"
+  },
+
+  weather_subtitle_4: {
+    id: "weather.subtitle_4",
+    defaultMessage: "This is where the short descriptive line will go here to describe the category"
+  },
+
+
+
+  department: {
+    id: 'data.field.department',
+    defaultMessage: 'Department',
+    description: 'Department Data Field'
+  },
+
+  year: {
+    id: 'data.field.year',
+    defaultMessage: 'Year',
+    description: 'Year Data Field'
+  },
+
+  weekly_consumption: {
+    id: 'data.field.weekly_consumption',
+    defaultMessage: 'Weekly Consumption',
+    description: 'Weekly Consumption Data Field'
+  },
+
+  house_hold_size: {
+    id: 'data.field.house_hold_size',
+    defaultMessage: 'House Hold Size ',
+    description: 'House Hold Size Field'
+  },
+
+
+  daily_consumption: {
+    id: 'data.field.daily_consumption',
+    defaultMessage: 'Weekly Consumption',
+    description: 'Weekly Consumption Data Field'
+  },
+
+  crop_type: {
+    id: 'data.field.crop_type',
+    defaultMessage: 'Crop Type',
+    description: 'Crop Type Data Field'
+  },
+  area: {
+    id: 'data.field.area',
+    defaultMessage: 'Area',
+    description: 'Area Data Field'
+  },
+  production: {
+    id: 'data.field.production',
+    defaultMessage: 'Production',
+    description: 'Production Data Field'
+  },
+
+  yield: {
+    id: 'data.field.yield',
+    defaultMessage: 'Yield',
+    description: 'Yield Data Field'
+  },
+  region_code: {
+    id: 'data.field.region_code',
+    defaultMessage: 'Region Code',
+    description: 'Region Code Data Field'
+  },
+  region: {
+    id: 'data.field.region',
+    defaultMessage: 'Region',
+    description: 'Region Data Field'
+  },
+
+  market: {
+    id: 'data.field.market',
+    defaultMessage: 'Market',
+    description: 'Market Data Field'
+  },
+  market: {
+    id: 'data.field.market',
+    defaultMessage: 'Market',
+    description: 'Market Data Field'
+  },
+
+  whole_sale_buy_price: {
+    id: 'data.field.whole_sale_buy_price',
+    defaultMessage: 'Whole Sale Buy Price',
+    description: 'Whole Sale Buy Price Data Field'
+  },
+  selling_price: {
+    id: 'data.field.selling_price',
+    defaultMessage: 'Selling Price',
+    description: 'Selling Price Data Field'
+  },
+  retail_buying_price: {
+    id: 'data.field.retail_buying_price',
+    defaultMessage: 'Retail Buying Price',
+    description: 'Selling Price Data Field'
+  },
+  date: {
+    id: 'data.field.date',
+    defaultMessage: 'Date',
+    description: 'Date Data Field'
+  },
+  quantity: {
+    id: 'data.field.quantity',
+    defaultMessage: 'Quantity',
+    description: 'Quantity Data Field'
+  },
+
+  count: {
+    id: 'table.aggregators.count',
+    defaultMessage: 'Count',
+    description: 'Count aggregator'
+  },
+
+  count_unique_values: {
+    id: 'table.aggregators.count_unique_values',
+    defaultMessage: 'Count',
+    description: 'Count Unique Values'
+  },
+  list_unique_values: {
+    id: 'table.aggregators.list_unique_values',
+    defaultMessage: 'List Unique Values',
+    description: 'List Unique Values'
+  },
+
+  sum: {
+    id: 'table.aggregators.sum',
+    defaultMessage: 'Sum',
+    description: 'Sum'
+  },
+  integer_sum: {
+    id: 'table.aggregators.integer_sum',
+    defaultMessage: 'Integer Sum',
+    description: 'Integer Sum'
+  },
+
+  average: {
+    id: 'table.aggregators.average',
+    defaultMessage: 'Average',
+    description: 'Average'
+  },
+
+  median: {
+    id: 'table.aggregators.median',
+    defaultMessage: 'Median',
+    description: 'Median'
+  },
+
+  sample_variance: {
+    id: 'table.aggregators.sample_variance',
+    defaultMessage: 'Sample Variance',
+    description: 'Sample Variance'
+  },
+  sample_standard_deviation: {
+    id: 'table.aggregators.sample_standard_deviation',
+    defaultMessage: 'Sample Standard Deviation',
+    description: 'Sample Standard Deviation'
+  },
+  minimum: {
+    id: 'table.aggregators.minimum',
+    defaultMessage: 'Minimum',
+    description: 'Minimum'
+  },
+
+  maximum: {
+    id: 'table.aggregators.maximum',
+    defaultMessage: 'Maximum',
+    description: 'Maximum'
+  },
+
+  first: {
+    id: 'table.aggregators.first',
+    defaultMessage: 'First',
+    description: 'First'
+  },
+  last: {
+    id: 'table.aggregators.last',
+    defaultMessage: 'Last',
+    description: 'Last'
+  },
+
+  sum_over_sum: {
+    id: 'table.aggregators.sum_over_sum',
+    defaultMessage: 'Sum over Sum',
+    description: 'Sum over Sum'
+  },
+
+  sum_as_fraction_of_total: {
+    id: 'table.aggregators.sum_as_fraction_of_total',
+    defaultMessage: 'Sum as Fraction of Total',
+    description: 'Sum as Fraction of Total'
+  },
+  sum_as_fraction_of_rows: {
+    id: 'table.aggregators.sum_as_fraction_of_rows',
+    defaultMessage: 'Sum as Fraction of Rows',
+    description: 'Sum as Fraction of Rows'
+  },
+  sum_as_fraction_of_columns: {
+    id: 'table.aggregators.sum_as_fraction_of_columns',
+    defaultMessage: 'Sum as Fraction of Columns',
+    description: 'Sum as Fraction of Columns'
+  },
+  count_as_fraction_of_total: {
+    id: 'table.aggregators.count_as_fraction_of_total',
+    defaultMessage: 'Count as Fraction of Total',
+    description: 'Count as Fraction of Total'
+  },
+
+  count_as_fraction_of_rows: {
+    id: 'table.aggregators.count_as_fraction_of_rows',
+    defaultMessage: 'Count as Fraction of Rows',
+    description: 'Count as Fraction of Rows'
+  },
+
+  count_as_fraction_of_columns: {
+    id: 'table.aggregators.count_as_fraction_of_columns',
+    defaultMessage: 'Count as Fraction of Columns',
+    description: 'Count as Fraction of Columns'
+  }
+
+});
+
+
+
+export default messages
