0bc9b4898f8f3a24308d8887581ed3a8ba7d1069
dbianco <dbianco@developmentgateway.org>
2019-11-05 16:13:24 -0300

    Add percentage to poverty summary

--- persistence/src/main/java/org/devgateway/toolkit/persistence/dto/PovertySummary.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/dto/PovertySummary.java
@@ -1,24 +1,43 @@
 package org.devgateway.toolkit.persistence.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.devgateway.toolkit.persistence.dao.Region;
 import org.devgateway.toolkit.persistence.dao.categories.PovertyLevel;
 
 public class PovertySummary {
+
     private long count;
+
+    @JsonIgnore
+    private long povertyLevelId;
+
     private String povertyLevel;
+
     private String povertyLevelFr;
+
+    @JsonIgnore
+    private long regionId;
+
     private String region;
+
     private String regionFr;
+
     private int year;
+
+    private double percentage;
 
     public PovertySummary(Object[] objects) {
         if (objects != null && objects.length > 3) {
             this.count = (long) objects[0];
             PovertyLevel povertyLevel = (PovertyLevel) objects[1];
+            this.povertyLevelId = povertyLevel.getId();
             this.povertyLevel = povertyLevel.getLabel();
             this.povertyLevelFr = povertyLevel.getLabelFr();
             this.year = (int) objects[2];
-            this.region = objects[3].toString();
-            this.regionFr = objects[3].toString();
+            Region region = (Region) objects[3];
+            this.regionId = region.getId();
+            this.region = region.getName();
+            this.regionFr = region.getName();
         }
     }
 
@@ -68,5 +87,29 @@ public class PovertySummary {
 
     public void setRegionFr(String regionFr) {
         this.regionFr = regionFr;
+    }
+
+    public long getPovertyLevelId() {
+        return povertyLevelId;
+    }
+
+    public void setPovertyLevelId(long povertyLevelId) {
+        this.povertyLevelId = povertyLevelId;
+    }
+
+    public long getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(long regionId) {
+        this.regionId = regionId;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
     }
 }
