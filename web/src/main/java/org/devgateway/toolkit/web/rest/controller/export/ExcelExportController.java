ec41713e175ba2cabc18b3c728639b1dc266b56a
sdimunzio <sdimunzio@gmail.com>
2019-12-04 11:51:54 -0300

    check style issue

--- web/src/main/java/org/devgateway/toolkit/web/rest/controller/export/ExcelExportController.java
+++ web/src/main/java/org/devgateway/toolkit/web/rest/controller/export/ExcelExportController.java
@@ -3,7 +3,12 @@ package org.devgateway.toolkit.web.rest.controller.export;
 import io.swagger.annotations.ApiOperation;
 import org.devgateway.toolkit.web.rest.controller.filter.IndicatorFilterPagingRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
