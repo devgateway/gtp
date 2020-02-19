1eac7b2319f131682356243ad39cc6c882eda276
dbianco <dbianco@developmentgateway.org>
2020-02-19 16:42:31 -0300

    AD3-354 Add image size text in Rapid Links

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/EditRapidLinkPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/edit/EditRapidLinkPage.java
@@ -16,6 +16,7 @@ package org.devgateway.toolkit.forms.wicket.page.edit;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
@@ -110,6 +111,9 @@ public class EditRapidLinkPage extends AbstractEditPage<RapidLink> {
                 getString("filenameError")));
         fileInput.required();
         editForm.add(fileInput);
+        editForm.add(new Label("imageInstructions", new StringResourceModel("imageInstructions", this, null))
+                .setEscapeModelStrings(false));
+
     }
 
     @Override
