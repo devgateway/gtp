6a1b284e986a1535fd1baec9e851e8045f953f04
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-04-02 20:56:12 +0300

    ANACIM-15 organization persistence

--- persistence/src/main/java/org/devgateway/toolkit/persistence/dao/categories/Organization.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/dao/categories/Organization.java
@@ -11,26 +11,59 @@
  *******************************************************************************/
 package org.devgateway.toolkit.persistence.dao.categories;
 
-import javax.persistence.Entity;
-
+import org.devgateway.toolkit.persistence.dao.AbstractAuditableEntity;
+import org.devgateway.toolkit.persistence.dao.Labelable;
+import org.devgateway.toolkit.persistence.dao.OrganizationTag;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.io.Serializable;
 
 /**
  * 
  * @author mpostelnicu
  *
  */
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @Entity
 @Audited
-public class Organization extends Category {
+public class Organization extends AbstractAuditableEntity implements Serializable, Labelable {
 
-    public Organization() {
-        super();
+    @Enumerated(EnumType.STRING)
+    private OrganizationTag tag;
+
+    private String label;
+
+    public OrganizationTag getTag() {
+        return tag;
     }
 
-    public Organization(final String label) {
-        super();
+    public void setTag(OrganizationTag tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public void setLabel(String label) {
         this.label = label;
+    }
+
+    @Override
+    public String getLabel() {
+        return label;
+    }
+
+    @Override
+    public String getLabel(String lang) {
+        return label;
+    }
+
+    @Override
+    public AbstractAuditableEntity getParent() {
+        return null;
     }
 
 }
