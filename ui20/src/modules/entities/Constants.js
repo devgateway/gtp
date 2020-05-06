8866d5e0ae978a44eef7213ba3ea7fe7183639e7
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-05-06 13:37:23 +0300

    ANACIM-39 generate rainfall chart data

---
+++ ui20/src/modules/entities/Constants.js
@@ -0,0 +1,5 @@
+export const MONTHS = [5, 6, 7, 8, 9, 10]
+export const DECADALS = [1, 2, 3]
+// nivo doesn't support rendering 0, though some issues & PRs exist
+export const ZERO_VALUE = -1
+export const NA_VALUE = -2
--- ui20/src/redux/actions/waterActions.js
+++ ui20/src/redux/actions/waterActions.js
@@ -1,6 +1,8 @@
 import * as api from "../../modules/api/index";
+import * as C from "../../modules/entities/Constants";
 import CommonConfig from "../../modules/entities/rainfall/CommonConfig";
 import RainLevelChart from "../../modules/entities/rainfall/RainLevelChart";
+import messages from "../../translations/messages";
 import {WATER_RESOURCES} from "../reducers/Water";
 
 export const loadAllWaterData = () => (dispatch, getState) => {
@@ -17,3 +19,51 @@ const transformAll = (allData) => {
     rainLevelChart: new RainLevelChart(rainLevelChart)
   }
 }
+
+export const getRain = (intl) => (dispatch, getState) => {
+  const {rainLevelChart} = getState().getIn(['water', 'data'])
+  const keys = rainLevelChart.filter.years.sort().reverse()
+  const barData = []
+  const { byDecadal } = rainLevelChart.setting
+  const indexBy = byDecadal ? 'monthDecadal' : 'month'
+
+  C.MONTHS.forEach(month => {
+    const monthLabel = `${intl.formatMessage(messages[`month_${month}`])}`
+    if (byDecadal) {
+      C.DECADALS.forEach(decadal => {
+        const record = {}
+        record[indexBy] = `${monthLabel},${decadal}`
+        keys.forEach(year => {
+          record[year] = asChartValue(rainLevelChart.data.getDecadalLevel(year, month, decadal))
+        })
+        barData.push(record)
+      })
+    } else {
+      const record = {}
+      record[indexBy] = monthLabel
+      keys.forEach(year => {
+        record[year] = asChartValue(rainLevelChart.data.getMonthLevel(year, month))
+      })
+      barData.push(record)
+    }
+  })
+
+  return {
+    data: barData,
+    keys: keys,
+    groupMode: 'grouped',
+    indexBy,
+    colors: {
+      // TODO update based on mockup
+      scheme: 'category10'
+    },
+    byDecadal
+  }
+}
+
+const asChartValue = (value) => {
+  if (value > 0) return value
+  if (value === undefined) return C.NA_VALUE
+  return C.ZERO_VALUE
+}
+
