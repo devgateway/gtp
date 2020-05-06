4f3f0fce7f6711d0291f6028495061194f9e3a31
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-05-06 13:23:06 +0300

    ANACIM-39 graphic layout component, integrated for rainfall

--- ui20/src/components/water/index.jsx
+++ ui20/src/components/water/index.jsx
@@ -1,13 +1,18 @@
-import React, {Component} from "react";
-import {injectIntl} from "react-intl";
-import {connect} from "react-redux";
-import {loadAllWaterData} from "../../redux/actions/waterActions";
-import * as PropTypes from "prop-types";
+import * as PropTypes from "prop-types"
+import React, {Component} from "react"
+import {injectIntl} from "react-intl"
+import {connect} from "react-redux"
+import * as waterActions from "../../redux/actions/waterActions"
+import Graphic from "../common/Graphic"
+import "../ipar/indicators/indicators.scss"
+
 
 class WaterResources extends Component {
 
   static propTypes = {
-    onLoadAll: PropTypes.func.isRequired
+    onLoadAll: PropTypes.func.isRequired,
+    getRain: PropTypes.func.isRequired,
+    isLoaded: PropTypes.bool.isRequired,
   }
 
   componentDidMount() {
@@ -15,8 +20,16 @@ class WaterResources extends Component {
   }
 
   render() {
-    return (<div className="png exportable">
-      <div style={ {color: 'black', 'text-align': 'center'}}>TODO</div>
+    const {isLoaded} = this.props;
+    if (!isLoaded) {
+      return <div></div>
+    }
+    return (<div className="indicators content fixed">
+      <Graphic
+        id="anchor.indicator.water.rainfall" titleId="indicators.chart.rainfall.title"
+        sourceId="indicators.chart.rainfall.source">
+
+      </Graphic>
     </div>)
   }
 
@@ -24,12 +37,12 @@ class WaterResources extends Component {
 
 const mapStateToProps = state => {
   return {
-
+    isLoaded: state.getIn(['water', 'isLoaded'])
   }
 }
 
 const mapActionCreators = {
-  onLoadAll: loadAllWaterData
+  onLoadAll: waterActions.loadAllWaterData
 }
 
 export default injectIntl(connect(mapStateToProps, mapActionCreators)(WaterResources))
