c29626012eba0d73aea5ebd56bc44d8380a87e46
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-05-22 17:41:33 +0300

    ANACIM-89 river level line chart

--- ui20/src/components/water/river/RiverLevel.jsx
+++ ui20/src/components/water/river/RiverLevel.jsx
@@ -1,17 +1,85 @@
-import React, {Component} from "react"
+import {ResponsiveLine} from "@nivo/line"
 import PropTypes from "prop-types"
+import React, {Component} from "react"
 import {injectIntl} from "react-intl"
 import {connect} from "react-redux"
+import RiverLevelChartDTO from "../../../modules/graphic/water/river/RiverLevelChartDTO"
 
 class RiverLevel extends Component {
   static propTypes = {
-
+    data: PropTypes.instanceOf(RiverLevelChartDTO).isRequired,
   }
 
   render() {
+    const { data, intl } = this.props
+
     return (
       <div className="png exportable chart container">
-        TODO
+        <ResponsiveLine
+          enableGridY={true}
+          enableGridX={true}
+          margin={{ top: 50, right: 50, bottom: 110, left: 60 }}
+
+          data={data.lines}
+          xScale={{
+            type: 'time',
+            format: '%Y-%m-%d',
+            precision: 'day',
+          }}
+          xFormat="time:%Y-%m-%d"
+          yScale={{
+            type: 'linear',
+            stacked: false,
+          }}
+          enableSlices='x'
+
+          axisLeft={{
+            legendPosition: 'middle',
+            legend: intl.formatMessage({ id: "water.rainlevel.level"}),
+            legendOffset: -50,
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+          }}
+          axisBottom={{
+            format: "%b",
+            tickValues: 'every month',
+            legend: intl.formatMessage({ id: "water.rainlevel.period"}),
+            legendOffset: 40,
+            legendPosition: 'middle'
+          }}
+          curve="monotoneX"
+          enablePoints={false}
+          useMesh={true}
+          colors={{ scheme: 'category10' }}
+          animate={true}
+
+          legends={[
+            {
+              anchor: 'bottom-left',
+              direction: 'row',
+              justify: false,
+              translateX: -11,
+              translateY: 70,
+              itemsSpacing: 0,
+              itemWidth: 120,
+              itemHeight: 20,
+              itemOpacity: 0.75,
+              symbolSize: 12,
+              symbolShape: 'circle',
+              symbolBorderColor: 'rgba(0, 0, 0, .5)',
+              effects: [
+                {
+                  on: 'hover',
+                  style: {
+                    itemBackground: 'rgba(0, 0, 0, .03)',
+                    itemOpacity: 1
+                  }
+                }
+              ]
+            }
+          ]}
+        />
       </div>);
   }
 }
--- ui20/src/components/water/river/index.jsx
+++ ui20/src/components/water/river/index.jsx
@@ -2,19 +2,23 @@ import * as PropTypes from "prop-types"
 import React, {Component} from "react"
 import {injectIntl} from "react-intl"
 import {connect} from "react-redux"
+import * as waterActions from "../../../redux/actions/waterActions"
 import Graphic from "../../common/Graphic"
 import RiverLevel from "./RiverLevel"
 
 
 class RiverLevelGraphic extends Component {
   static propTypes = {
+    getRiverLevel: PropTypes.func.isRequired,
   }
 
   render() {
+    const {getRiverLevel} = this.props
+
     return (<Graphic
       id="anchor.indicator.water.riverlevel" titleId="indicators.chart.riverlevel.title"
       sourceId="indicators.chart.riverlevel.source" className="rainfall">
-      <RiverLevel />
+      <RiverLevel {...getRiverLevel()} />
     </Graphic>)
   }
 }
@@ -26,6 +30,7 @@ const mapStateToProps = state => {
 }
 
 const mapActionCreators = {
+  getRiverLevel: waterActions.getRiverLevel
 }
 
 export default injectIntl(connect(mapStateToProps, mapActionCreators)(RiverLevelGraphic))
