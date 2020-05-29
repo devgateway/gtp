11d9a4ada4d29a5eb02114c52d319647a662b77e
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-05-29 20:42:05 +0300

    ANACIM-131 user ArcGisOnline world map attribution in market map

--- ui20/src/redux/actions/market/index.js
+++ ui20/src/redux/actions/market/index.js
@@ -1,13 +1,16 @@
 import * as api from "../../../modules/api"
 import AgricultureConfig from "../../../modules/entities/config/AgricultureConfig"
 import {MARKET_AND_AGRICULTURE} from "../../reducers/Agriculture"
+import * as appActions from "../appActions"
 
 
-export const loadAllMarketData = () => (dispatch, getState) =>
+export const loadAllMarketData = () => (dispatch, getState) => {
   dispatch({
     type: MARKET_AND_AGRICULTURE,
     payload: api.getAllMarketAndAgriculture().then(transformAll)
   })
+  appActions.loadWorldMapAttribution()(dispatch, getState)
+}
 
 const transformAll = (allData) => {
   const agricultureConfig = new AgricultureConfig(allData.agricultureConfig, allData.commonConfig)
