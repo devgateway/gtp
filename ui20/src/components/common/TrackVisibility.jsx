fd4754a334f9d6d8e3ef9789cd708d9ba828cbdb
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-06-04 20:53:48 +0300

    ANACIM-143 visibility tracking component

---
+++ ui20/src/components/common/TrackVisibility.jsx
@@ -0,0 +1,50 @@
+import React, {Component, createRef} from "react"
+import PropTypes from "prop-types"
+
+export default class TrackVisibility extends Component {
+  static propTypes = {
+    onVisible: PropTypes.func.isRequired,
+    thresholds: PropTypes.arrayOf(PropTypes.number).isRequired,
+  }
+
+  ref = createRef()
+
+  async componentDidMount() {
+    const ascThresholds = this.props.thresholds.sort()
+    const descThresholds = ascThresholds.reverse()
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        const threshold = descThresholds.find(t => t <= entry.intersectionRatio)
+        if (threshold) {
+          this.props.onVisible(threshold, entry.intersectionRatio)
+        }
+      }, {
+        root: null,
+        rootMargin: '0px',
+        threshold: ascThresholds,
+      }
+    )
+
+    if (this.ref.current) {
+      observer.observe(this.ref.current)
+    }
+  }
+
+  render() {
+    return <div ref={this.ref}>{this.props.children}</div>
+  }
+
+}
+
+let initialized = false
+
+export async function initializeObserver() {
+  if (!initialized) {
+    initialized = true
+    if (!('IntersectionObserver' in window)) {
+      console.log('Loading IntersectionObserver polifill')
+      await import('intersection-observer')
+    }
+  }
+}
