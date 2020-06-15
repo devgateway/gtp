2251adf97052ca0ff7633b4dd65b7b74b93b1fb2
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-06-15 17:49:48 +0300

    ANACIM-189 dropdown icon alignment

--- ui20/src/components/ComponentUtil.js
+++ ui20/src/components/ComponentUtil.js
@@ -1,3 +1,10 @@
 export const cssClasses = (...classes) => classes.filter(c => !!c).join(' ')
 
 export const scrollToRef = (ref, extraOffset) => window.scrollTo(0, ref.current.offsetTop + extraOffset)
+
+export const getBrowserClass = () => {
+  if (!!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)) {
+    return 'chrome'
+  }
+  return ''
+}
