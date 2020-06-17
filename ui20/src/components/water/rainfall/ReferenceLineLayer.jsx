9b8f3b12f82f451f84c28aa5cee08f4d99195111
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-06-17 17:47:06 +0300

    ANACIM-219 mirror left 2nd half of refs tooltips to fit small screen

--- ui20/src/components/water/rainfall/ReferenceLineLayer.jsx
+++ ui20/src/components/water/rainfall/ReferenceLineLayer.jsx
@@ -6,6 +6,9 @@ import {Tooltip} from 'react-svg-tooltip'
 import * as sccJS from "../../css"
 import {INNER_PADDING} from "./RainfallGraphicConstants"
 
+const tooltipShift = 2
+const tooltipWidth = 135
+
 class ReferenceLine extends Component {
   static propTypes = {
     bars: PropTypes.array.isRequired,
@@ -35,6 +38,8 @@ class ReferenceLine extends Component {
             const value = b.data.data.lineValues.get(b.data.id)
             const xValue = value === undefined ? intl.formatMessage({ id: "all.graphic.value.NA" }) :
               intl.formatNumber(value, {minimumFractionDigits: 0, maximumFractionDigits: 1})
+            const x = (bars.length / 2 <= index) ? -tooltipShift - tooltipWidth : tooltipShift
+
             return (
               <g key={b.data.index}>
                 <circle
@@ -49,8 +54,8 @@ class ReferenceLine extends Component {
                     <feGaussianBlur result="blurOut" in="offOut" stdDeviation="3" />
                     <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
                   </filter>
-                  <rect x={2} y={2} width={120} height={35} rx={.5} ry={.5} strokeWidth={1} fill="white" filter="url(#f1)" />
-                  <text x={10} y={23} fontSize={12} fill='#747474'>
+                  <rect x={x} y={2} width={tooltipWidth} height={35} rx={.5} ry={.5} strokeWidth={1} fill="white" filter="url(#f1)" />
+                  <text x={x + 10} y={23} fontSize={12} fill='#747474'>
                     <tspan className="color" style={{backgroundColor: b.data.color, width: 6, fontWeight: 800, fontSize: 20}}>|</tspan>
                     <tspan>{`${b.data.data.indexLabel} : ${xValue} ${unit}`}</tspan>
                   </text>
