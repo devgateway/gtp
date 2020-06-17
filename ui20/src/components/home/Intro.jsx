1ca404ed4381ff40424a42c41dd0fdf7e7d1031f
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-06-17 18:36:27 +0300

    ANACIM-218 homepage text updates

--- ui20/src/components/home/Intro.jsx
+++ ui20/src/components/home/Intro.jsx
@@ -38,7 +38,7 @@ const Header = (props)=> {
         <img className="logo" src="logo-anacim-standard-optimized.png" alt={''}/>
         <IntroTitle />
         <IntroText />
-        <PresentationTitle />
+        {/* <PresentationTitle /> */}
         <PresentationText />
       </MainIntro>
     </div>)
