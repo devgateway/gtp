c81c7806f7f30085076192aa13159b0442b2212c
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-07-28 01:21:03 +0300

    ANACIM-251 initial GTP members page

---
+++ ui20/src/components/about/index.jsx
@@ -0,0 +1,42 @@
+import * as PropTypes from "prop-types"
+import React, {Component} from "react"
+import {injectIntl} from "react-intl"
+import {connect} from "react-redux"
+import MemberData from "../../modules/entities/member/MemberData"
+import * as memberActions from "../../redux/actions/memberActions"
+
+class About extends Component {
+  static propTypes = {
+    onLoadAll: PropTypes.func.isRequired,
+    memberData: PropTypes.objectOf(MemberData),
+  }
+
+  componentDidMount() {
+    this.props.onLoadAll();
+  }
+
+  render() {
+    const {isLoaded} = this.props
+    if (!isLoaded) {
+      return <div/>
+    }
+
+    const memberData: MemberData = this.props.memberData
+
+    return 'TODO'
+  }
+
+}
+
+const mapStateToProps = state => {
+  return {
+    isLoaded: state.getIn(['member', 'isLoaded']),
+    memberData: state.getIn(['member', 'data']),
+  }
+}
+
+const mapActionCreators = {
+  onLoadAll: memberActions.loadAllMembers,
+}
+
+export default injectIntl(connect(mapStateToProps, mapActionCreators)(About))
