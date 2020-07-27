6800fa39d801dc1e02263c1c9c3afc38edf87111
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-07-27 19:02:35 +0300

    ANACIM-292 scroll top on page load

--- ui20/src/components/layout/Layout.jsx
+++ ui20/src/components/layout/Layout.jsx
@@ -1,22 +1,28 @@
-import React, {Component} from "react"
+import React, {useEffect} from "react"
+import {useLocation} from "react-router"
 import {Grid, GridColumn, GridRow} from "semantic-ui-react";
 import "./layout.scss"
 import Menu from "./menu"
 
-export default class Layout extends Component {
+const Layout = (props) => {
+  const location = useLocation()
 
-  render() {
-    return (
-      <Grid className="page-layout" stackable={false}>
-        <GridRow>
-          <GridColumn className="page-menu menu">
-            <Menu/>
-          </GridColumn>
-          <GridColumn className="page-content">
-            {this.props.children}
-          </GridColumn>
-        </GridRow>
-      </Grid>
-    );
-  }
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location])
+
+  return (
+    <Grid className="page-layout" stackable={false}>
+      <GridRow>
+        <GridColumn className="page-menu menu">
+          <Menu/>
+        </GridColumn>
+        <GridColumn className="page-content">
+          {props.children}
+        </GridColumn>
+      </GridRow>
+    </Grid>
+  );
 }
+
+export default Layout
