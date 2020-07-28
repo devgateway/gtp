33e0ddc89b537141c08b6c9a0beab0540dd714ef
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-07-28 15:04:20 +0300

    ANACIM-283 app pallet changes; river level chart colors

--- ui20/src/components/css.js
+++ ui20/src/components/css.js
@@ -10,6 +10,7 @@ export const GRAPHIC_COLOR_GRAY1 = '#898989'
 export const GRAPHIC_COLOR_GRAY2 = '#c1c1c1'
 export const GRAPHIC_COLOR_GRAY3 = '#e1e1e1'
 export const GRAPHIC_COLOR_ORANGE = '#e86d00'
+export const GRAPHIC_LIGHT_PLUM = '#D2A6E4'
 
 export const GRAPHIC_MAIN_COLORS = [
   GRAPHIC_COLOR_BLUE,
@@ -20,8 +21,26 @@ export const GRAPHIC_MAIN_COLORS = [
 
 const { category10, set3, dark2 } = categoricalColorSchemes
 // category10=10, set3=12, dark2=8 => total 30 colors
-export const PALLET_COLORS = [...category10, ...set3, ...dark2]
-export const REFERENCE_COLORS = ['#bdbdbd', '#828282', '#535353', '#0e0e0e']
+export const PALLET_COLORS_NIVO = [...category10, ...set3, ...dark2]
+export const REFERENCE_COLORS_OLD = ['#bdbdbd', '#828282', '#535353', '#0e0e0e']
+export const REFERENCE_COLORS = [GRAPHIC_LIGHT_PLUM, GRAPHIC_COLOR_BLUE]
+
+export const PALLET_COLORS = [
+  GRAPHIC_COLOR_BLUE,
+  GRAPHIC_COLOR_RED,
+  GRAPHIC_COLOR_YELLOW,
+  GRAPHIC_COLOR_ORANGE,
+  GRAPHIC_LIGHT_PLUM,
+  "#FF8080",
+  "#2E5B92",
+  "#A05EBC",
+  "#93E155",
+  "#79BEFE",
+  "#29BEA4",
+  "#479519"
+]
+
+export const RIVER_PALLET_COLORS = PALLET_COLORS.filter(c => !REFERENCE_COLORS.includes(c) && c !== GRAPHIC_COLOR_RED)
 
 export const NIVO_THEME = {
   axis: {
