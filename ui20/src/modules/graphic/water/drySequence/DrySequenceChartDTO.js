2be758bec1352a9821495c0fc2fa625ccdbedf4b
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-07-31 11:49:11 +0300

    ANACIM-309 no data in dry sequence chart

--- ui20/src/modules/graphic/water/drySequence/DrySequenceChartDTO.js
+++ ui20/src/modules/graphic/water/drySequence/DrySequenceChartDTO.js
@@ -5,12 +5,14 @@ export default class DrySequenceChartDTO {
   groupMode: string
   colors: Array<string>
   isDaysWithRain: boolean
+  hasData: boolean
 
-  constructor(indexBy, keys, barData, isDaysWithRain) {
+  constructor(indexBy, keys, barData, isDaysWithRain, hasData: boolean) {
     this.indexBy = indexBy
     this.keys = keys
     this.barData = barData
     this.groupMode = 'grouped'
     this.isDaysWithRain = isDaysWithRain
+    this.hasData = hasData
   }
 }
