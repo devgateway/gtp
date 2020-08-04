6ca31c321067ca5ee8d69cca21ff0775e5f09ca9
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-08-04 23:04:32 +0300

    ANACIM-277 support for a function lambda with 3 params

---
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/util/Function4.java
@@ -0,0 +1,8 @@
+package org.devgateway.toolkit.persistence.util;
+
+import java.io.Serializable;
+
+@FunctionalInterface
+public interface Function4<Param1Type, Param2Type, Param3Type, ReturnType> extends Serializable {
+    public ReturnType apply(Param1Type param1, Param2Type param2, Param3Type param3);
+}
