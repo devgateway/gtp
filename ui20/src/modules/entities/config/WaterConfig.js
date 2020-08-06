42923877a8f5dc6b3c64a1d61e83ea8c1a40c9f2
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-08-06 12:52:26 +0300

    ANACIM-279 common water res. fallback changes; rainfall chart fallback

--- ui20/src/modules/entities/config/WaterConfig.js
+++ ui20/src/modules/entities/config/WaterConfig.js
@@ -5,7 +5,7 @@ import CommonConfig, {fromApiToMap} from "./CommonConfig"
 export default class WaterConfig extends CommonConfig {
   zones: Map<number, Zone>
 
-  constructor({posts}, commonConfig) {
+  constructor({posts} = {}, commonConfig = {}) {
     super(commonConfig);
     this.posts = fromApiToMap(posts, PluviometricPost.newInstance, 'department', this.departments)
   }
