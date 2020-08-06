4838d0540b421e79af94c6de3684294cae08092d
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-08-06 13:52:59 +0300

    ANACIM-279 market location  fallback for loading, no data and error data

--- ui20/src/components/market/marketLocation/index.jsx
+++ ui20/src/components/market/marketLocation/index.jsx
@@ -2,20 +2,28 @@ import React, {Component} from "react"
 import {injectIntl} from "react-intl"
 import {connect} from "react-redux"
 import Graphic from "../../common/graphic/Graphic"
+import GraphicWithFallback from "../../common/graphic/GraphicWithFallback"
 import MarketMap from "./MarketMap"
 
+const hasDataFunc = ({agricultureConfig}) => !!agricultureConfig
+const childPropsBuilder = (props) => props
+const MarketLocationGraphicWithFallback = GraphicWithFallback('agriculture', 'isLoading', 'isLoaded',
+  childPropsBuilder, hasDataFunc)
 
 class MarketLocationGraphic extends Component {
   static propTypes = {
   }
 
   render() {
+    const {agricultureConfig} = this.props
     return (
       <Graphic
         id="anchor.indicator.agriculture.market.map" titleId="indicators.map.market.title"
         helpId="indicators.map.market.help" helpProps={{wide: true}}
         className="map-graphic">
-        <MarketMap />
+        <MarketLocationGraphicWithFallback agricultureConfig={agricultureConfig}>
+          {childProps => <MarketMap />}
+        </MarketLocationGraphicWithFallback>
       </Graphic>)
   }
 }
@@ -23,6 +31,7 @@ class MarketLocationGraphic extends Component {
 
 const mapStateToProps = state => {
   return {
+    agricultureConfig: state.getIn(['agriculture', 'data', 'agricultureConfig']),
   }
 }
 
