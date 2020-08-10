70135d782f1ff2519f34121ffd62f8becbe6feb4
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-08-10 19:47:08 +0300

    ANACIM-321 livestock disease repository, service and initial values

---
+++ persistence/src/main/resources/sql/livestock-diseases.sql
@@ -0,0 +1,7 @@
+INSERT INTO category(id, dtype, name) VALUES (nextval('hibernate_sequence'), 'LivestockDisease', 'Maladie hémorragique du lapin');
+INSERT INTO category(id, dtype, name) VALUES (nextval('hibernate_sequence'), 'LivestockDisease', 'Maladie de Newcastle');
+INSERT INTO category(id, dtype, name) VALUES (nextval('hibernate_sequence'), 'LivestockDisease', 'Pasteurellose ovine');
+INSERT INTO category(id, dtype, name) VALUES (nextval('hibernate_sequence'), 'LivestockDisease', 'Distomatose');
+INSERT INTO category(id, dtype, name) VALUES (nextval('hibernate_sequence'), 'LivestockDisease', 'Maladie de Gumboro');
+INSERT INTO category(id, dtype, name) VALUES (nextval('hibernate_sequence'), 'LivestockDisease', 'Peste des petits ruminants');
+INSERT INTO category(id, dtype, name) VALUES (nextval('hibernate_sequence'), 'LivestockDisease', 'Peste porcine africaine');
