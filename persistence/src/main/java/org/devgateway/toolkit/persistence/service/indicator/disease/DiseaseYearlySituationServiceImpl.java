ea504dcb135a70d43ae6ebd05851e1733be4a0e6
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-08-20 12:00:45 +0300

    ANACIM-322 leave template completely empty (no sample region)

--- persistence/src/main/java/org/devgateway/toolkit/persistence/service/indicator/disease/DiseaseYearlySituationServiceImpl.java
+++ persistence/src/main/java/org/devgateway/toolkit/persistence/service/indicator/disease/DiseaseYearlySituationServiceImpl.java
@@ -1,24 +1,17 @@
 package org.devgateway.toolkit.persistence.service.indicator.disease;
 
-import org.devgateway.toolkit.persistence.dao.indicator.DiseaseQuantity;
 import org.devgateway.toolkit.persistence.dao.indicator.DiseaseYearlySituation;
-import org.devgateway.toolkit.persistence.dao.location.Region;
 import org.devgateway.toolkit.persistence.repository.indicator.DiseaseYearlySituationRepository;
 import org.devgateway.toolkit.persistence.repository.norepository.BaseJpaRepository;
 import org.devgateway.toolkit.persistence.service.AdminSettingsService;
 import org.devgateway.toolkit.persistence.service.BaseJpaServiceImpl;
-import org.devgateway.toolkit.persistence.service.category.LivestockDiseaseService;
-import org.devgateway.toolkit.persistence.service.location.RegionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.time.Month;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -34,12 +27,6 @@ public class DiseaseYearlySituationServiceImpl extends BaseJpaServiceImpl<Diseas
 
     @Autowired
     private AdminSettingsService adminSettingsService;
-
-    @Autowired
-    private RegionService regionService;
-
-    @Autowired
-    private LivestockDiseaseService livestockDiseaseService;
 
     @Autowired
     private EntityManager entityManager;
@@ -77,14 +64,7 @@ public class DiseaseYearlySituationServiceImpl extends BaseJpaServiceImpl<Diseas
 
     @Override
     public DiseaseYearlySituation getExample(Integer year) {
-        DiseaseYearlySituation situation = new DiseaseYearlySituation(year);
-        Region region = regionService.findAll(PageRequest.of(0, 1, Sort.by("name"))).iterator().next();
-
-        livestockDiseaseService.findAll(Sort.by("name")).stream().map(livestockDisease ->
-                new DiseaseQuantity(region, livestockDisease, Month.JANUARY, null))
-                .forEach(situation::addDiseaseQuantity);
-
-        return situation;
+        return new DiseaseYearlySituation(year);
     }
 
     @Override
