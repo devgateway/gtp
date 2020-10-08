cffacc2710538a8b1293f17975caa29d981b9b7c
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-10-08 22:57:47 +0300

    ANACIM-380 cnsc header logo API

--- web/src/main/java/org/devgateway/toolkit/web/rest/controller/FileMetadataController.java
+++ web/src/main/java/org/devgateway/toolkit/web/rest/controller/FileMetadataController.java
@@ -11,12 +11,22 @@
  *******************************************************************************/
 package org.devgateway.toolkit.web.rest.controller;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 import io.swagger.annotations.ApiOperation;
 import org.devgateway.toolkit.persistence.dao.FileMetadata;
 import org.devgateway.toolkit.persistence.service.FileMetadataService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,9 +36,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by Daniel Oliva
@@ -75,6 +82,29 @@ public class FileMetadataController {
         } catch (Exception e) {
             LOGGER.error("Error: " + e);
             throw new BadRequestException("File not found");
+        }
+    }
+
+    public static ResponseEntity<Resource> responseForFileMetadata(FileMetadata metadata, String ifNoneMatch) {
+        String contentDispositionValue = ContentDisposition.builder("attachment")
+                .filename(metadata.getName())
+                .build()
+                .toString();
+
+        String etag = "\"" + metadata.getId().toString() + "\"";
+
+        if (ifNoneMatch != null && (ifNoneMatch.equals("*") || ifNoneMatch.equals(etag))) {
+            return ResponseEntity.status(HttpStatus.NOT_MODIFIED)
+                    .eTag(etag)
+                    .build();
+        } else {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .eTag(etag)
+                    .cacheControl(CacheControl.noCache().mustRevalidate())
+                    .contentType(MediaType.parseMediaType(metadata.getContentType()))
+                    .contentLength(metadata.getSize())
+                    .header("Content-Disposition", contentDispositionValue)
+                    .body(new ByteArrayResource(metadata.getContent().getBytes()));
         }
     }
 }
