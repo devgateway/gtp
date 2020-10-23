021ffa7c1f321d6886783aa9d376f516a8506b90
nmandrescu <17854915+nmandrescu@users.noreply.github.com>
2020-10-23 18:33:53 +0300

    ANACIM-378 public home page link for non-authenticated users

--- forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/error/NotFoundPage.java
+++ forms/src/main/java/org/devgateway/toolkit/forms/wicket/page/error/NotFoundPage.java
@@ -1,6 +1,9 @@
 package org.devgateway.toolkit.forms.wicket.page.error;
 
+import org.apache.wicket.markup.html.link.AbstractLink;
+import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.devgateway.toolkit.forms.security.SecurityUtil;
 import org.wicketstuff.annotation.mount.MountPath;
 
 /**
@@ -12,5 +15,13 @@ public class NotFoundPage extends ErrorPage {
 
     public NotFoundPage(final PageParameters parameters) {
         super(parameters);
+    }
+
+    @Override
+    protected AbstractLink getLink() {
+        if (SecurityUtil.isAuthenticated()) {
+            return super.getLink();
+        }
+        return new ExternalLink("link", "/");
     }
 }
